f7bb16838d32f62fbbdc6a8a9d6c5a1bf4be6b1c
Evorah78 <info@evorah.co.uk>
2026-09-01 15:03:30 +0100

    Refresh Forced Self Care app cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='forced-self-care-v2';
+const CACHE='forced-self-care-v3';
 const ASSETS=['./','index.html','manifest.webmanifest','icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
