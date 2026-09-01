07b0ca72ecf8cba0498e961e9e9541be43e8b35a
Evorah78 <info@evorah.co.uk>
2026-09-01 10:19:17 +0100

    Add offline app service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='forced-self-care-v1';
+const ASSETS=['./','index.html','manifest.webmanifest','icon.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener('fetch',event=>{if(event.request.method==='GET')event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)))});
